26d20e92241fccbebb2b03a577f2375981f1ebe3
mammadagha <khudaverdiev11@gmail.com>
2021-03-30 01:31:17 +0400

    js 1

---
+++ js task1/js/main.js
@@ -0,0 +1,24 @@
+function myFunc(){
+    document.getElementById('button').innerHTML="Doğrudur";
+}
+function myFunc2(){
+    document.getElementById('button2').innerHTML="Səhvdir";
+}
+function myFunc3(){
+    document.getElementById('button3').innerHTML="Doğrudur";
+}
+function myFunc4(){
+    document.getElementById('button4').innerHTML="Səhvdir";
+}
+function myFunc5(){
+    document.getElementById('button5').innerHTML="Doğrudur";
+}
+function myFunc6(){
+    document.getElementById('button6').innerHTML="Səhvdir";
+}
+function myFunc7(){
+    document.getElementById('button7').innerHTML="Doğrudur";
+}
+function myFunc8(){
+    document.getElementById('button8').innerHTML="Səhvdir";
+}
