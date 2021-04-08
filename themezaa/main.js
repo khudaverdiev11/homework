c75641faec41c8794472e5f7ecb48e9b562af842
mammadagha <khudaverdiev11@gmail.com>
2021-04-08 11:25:24 +0400

    themezaa

---
+++ themezaa/main.js
@@ -0,0 +1,20 @@
+function bigImg() {
+    document.getElementById("left1").style.display = "block";
+    document.getElementById("left2").style.display = "block";
+    document.getElementById("left3").style.display = "block";
+  }
+  function normalImg() {
+    document.getElementById("left1").style.display = "none";
+    document.getElementById("left2").style.display = "none";
+    document.getElementById("left3").style.display = "none";
+  }
+  function bigImg1() {
+    document.getElementById("right1").style.display = "block";
+    document.getElementById("right2").style.display = "block";
+    document.getElementById("right3").style.display = "block";
+  }
+  function normalImg1() {
+    document.getElementById("right1").style.display = "none";
+    document.getElementById("right2").style.display = "none";
+    document.getElementById("right3").style.display = "none";
+  }
